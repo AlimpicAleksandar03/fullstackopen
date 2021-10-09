ed67e3d8ec6f3b6371d346afe877cdcdfee432d0
Aleksandar Alimpic <sasenija003@gmail.com>
2021-10-09 23:26:34 +0200

    part1

--- exercises/part1/part1/src/App.js
+++ exercises/part1/part1/src/App.js
@@ -1,51 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Header = (props) => {
-    return <h1>{props.course}</h1>;
-};
-const Part1 = (props) => {
-    return (
-        <p>
-            {props.part1} {props.exercises1}
-        </p>
-    );
-};
-const Part2 = (props) => {
-    return (
-        <p>
-            {props.part2} {props.exercises2}
-        </p>
-    );
-};
-const Part3 = (props) => {
-    return (
-        <p>
-            {props.part3} {props.exercises3}
-        </p>
-    );
-};
-const Total = (props) => {
-    const { exercises1, exercises2, exercises3 } = props;
-    const sum = exercises1 + exercises2 + exercises3;
-    return <p>Number of exercises {sum}</p>;
-};
-const Content = () => {
-    return (
-        <>
-            <Part1 part1='Fundamentals of React' exercises1={10} />
-            <Part2 part2='Using props to pass data' exercises2={7} />
-            <Part3 part3='State of a component' exercises3={14} />
-        </>
-    );
-};
 const App = () => {
+    const anecdotes = [
+        "If it hurts, do it more often",
+        "Adding manpower to a late software project makes it later!",
+        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "Premature optimization is the root of all evil.",
+        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+        "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+    ];
+    const [selected, setSelected] = useState(generateRandomIndex(anecdotes));
+    const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
+    const handleVote = () => {
+        const newPoints = [...points];
+        newPoints[selected] += 1;
+        setPoints(newPoints);
+    };
+    console.log(points);
+    const handleRandom = () => {
+        setSelected(generateRandomIndex(anecdotes));
+    };
     return (
         <div>
-            <Header course='Half Stack application development' />
-            <Total exercises1={10} exercises2={7} exercises3={14} />
-            <Content />
+            <h3> {anecdotes[selected]}</h3>
+            <Button handleClick={handleRandom} text='Next anecdote' />
+            <Button text='Vote' handleClick={handleVote} />
+            <div>{anecdotes[points.indexOf(Math.max(...points))]}</div>
         </div>
     );
 };
-
+const Button = ({ handleClick, text }) => {
+    return <button onClick={handleClick}>{text}</button>;
+};
+const generateRandomIndex = (arr) => {
+    return Math.floor(Math.random() * arr.length);
+};
+// const Winner = ({ anecdotes, totalVotes }) => {
+//     const winner = anecdotes[totalVotes.indexOf(Math.max(...points))];
+//     return <div>{winner}</div>;
+// };
 export default App;
