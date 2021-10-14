9d9f48fef898c4bed901762f80a4ccb70390f2af
Aleksandar Alimpic <sasenija003@gmail.com>
2021-10-14 21:12:42 +0200

    add part2

--- exercises/part2/exercises_2.6-2.10/src/App.js
+++ exercises/part2/exercises_2.6-2.10/src/App.js
@@ -1,36 +1,57 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    ]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
+    const names = persons.map((p) => p.name);
+    const updateNumber = (e) => {
+        const number = e.target.value;
+        setNewNumber(number);
+    };
+    console.log(names);
     const addPerson = (e) => {
         e.preventDefault();
-        const newPerson = { name: newName, id: persons.length + 1 };
-        setPersons(persons.concat(newPerson));
-        setNewName("");
+        const newPerson = {
+            name: newName,
+            id: persons.length + 1,
+            number: newNumber,
+        };
+        if (names.includes(newPerson.name)) {
+            alert(`${newPerson.name} is already added to phone book`);
+            setNewName("");
+            setNewNumber("");
+        } else {
+            setPersons(persons.concat(newPerson));
+            setNewName("");
+            setNewNumber("");
+        }
     };
     const updateName = (e) => {
         const name = e.target.value;
         setNewName(name);
     };
-    const names = persons.map((p) => p.name);
-    for (let name of names) {
-        if (names.indexOf(name) != names.lastIndexOf(name)) {
-            setPersons(
-                persons
-                    .slice(0, names.indexOf(name))
-                    .concat(persons.slice(names.indexOf(name) + 1)),
-            );
-            alert(`${name} is already added to phone book`);
-        }
-    }
-    console.log(persons);
+    const filter = (e) => {
+        const match = new RegExp(e.target.value, "i");
+        setPersons(persons.filter((person) => match.test(person.name)));
+    };
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                Filter: <input onChange={filter} />
+            </div>
             <form onSubmit={addPerson}>
                 <div>
                     name: <input value={newName} onChange={updateName} />
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={updateNumber} />
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -39,14 +60,22 @@ const App = () => {
             <h2>Numbers</h2>
             <div>
                 {persons.map((person) => (
-                    <Person name={person.name} key={person.id} />
+                    <Person
+                        name={person.name}
+                        number={person.number}
+                        key={person.id}
+                    />
                 ))}
             </div>
         </div>
     );
 };
-const Person = ({ name }) => {
-    return <div>{name}</div>;
+const Person = ({ name, number }) => {
+    return (
+        <div>
+            {name} {number}
+        </div>
+    );
 };
 
 export default App;
