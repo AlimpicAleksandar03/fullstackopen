53fdf1c41b2cfd21cc3bf8d1d530f7c68f09b6fe
Aleksandar Alimpic <sasenija003@gmail.com>
2021-10-16 16:01:16 +0200

    Extract parts into components

--- exercises/part2/exercises_2.6-2.10/src/App.js
+++ exercises/part2/exercises_2.6-2.10/src/App.js
@@ -7,19 +7,19 @@ const App = () => {
         { name: "Dan Abramov", number: "12-43-234345", id: 3 },
         { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
     ]);
-    const [filter, setFilter] = useState('');
+    const [filter, setFilter] = useState("");
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
-    const getMaxId = () => Math.max(...people.map(person => person.id));
+    const getMaxId = () => Math.max(...people.map((person) => person.id));
 
     const addPerson = (e) => {
         e.preventDefault();
 
         if (!newName || newName.length === 0) {
-            return alert('Name cannot be empty');
+            return alert("Name cannot be empty");
         }
         if (!newNumber || newNumber.length === 0) {
-            return alert('Number cannot be empty');
+            return alert("Number cannot be empty");
         }
 
         const newPerson = {
@@ -28,7 +28,7 @@ const App = () => {
             number: newNumber,
         };
 
-        if (people.find(({name}) => name === newPerson.name)) {
+        if (people.find(({ name }) => name === newPerson.name)) {
             alert(`${newPerson.name} is already added to phone book`);
         } else {
             setPeople([newPerson, ...people]);
@@ -49,36 +49,27 @@ const App = () => {
     const updateFilter = (event) => {
         const input = event.target.value;
         setFilter(input);
-    }
+    };
 
-    const peopleList = filter.length > 0 ? people.filter(({name}) => name.toLowerCase().includes(filter.toLowerCase())) : people;
+    const peopleList =
+        filter.length > 0
+            ? people.filter(({ name }) =>
+                  name.toLowerCase().includes(filter.toLowerCase()),
+              )
+            : people;
     return (
         <div>
             <h2>Phonebook</h2>
-            <label>
-                Filter: <input onChange={updateFilter} />
-            </label>
-            <form onSubmit={addPerson}>
-                <label>
-                    Name:
-                    <input value={newName} onChange={updateName} />
-                </label>
-                <label>
-                    Number:
-                    <input value={newNumber} onChange={updateNumber} />
-                </label>
-                <button type="submit">Add</button>
-            </form>
+            <Filter updateFilter={updateFilter} />
+            <PersonForm
+                name={newName}
+                number={newNumber}
+                onSubmit={addPerson}
+                updateName={updateName}
+                updateNumber={updateNumber}
+            />
             <h2>Numbers</h2>
-            <ul>
-                {peopleList.map((person) => (
-                    <Person
-                        name={person.name}
-                        number={person.number}
-                        key={person.id}
-                    />
-                ))}
-            </ul>
+            <People people={peopleList} />
         </div>
     );
 };
@@ -87,6 +78,39 @@ const Person = ({ name, number }) => {
         <li>
             {name} &mdash; {number}
         </li>
+    );
+};
+const People = ({ people }) => {
+    return (
+        <ul>
+            {people.map((person) => (
+                <Person
+                    name={person.name}
+                    number={person.number}
+                    key={person.id}
+                />
+            ))}
+        </ul>
+    );
+};
+const PersonForm = ({ name, number, updateName, updateNumber, onSubmit }) => {
+    return (
+        <form onSubmit={onSubmit}>
+            <label>
+                Name: <input value={name} onChange={updateName} />
+            </label>
+            <label>
+                Number: <input value={number} onChange={updateNumber} />
+            </label>
+            <button type="submit">Add</button>
+        </form>
+    );
+};
+const Filter = ({ updateFilter }) => {
+    return (
+        <label>
+            Filter: <input onChange={updateFilter} />
+        </label>
     );
 };
 
